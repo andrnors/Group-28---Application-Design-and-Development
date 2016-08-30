24cf5db366539e41fe8011832f3e9015cdc78221
Andreas Norstein <andreas.norstein@connect.qut.edu.au>
2016-08-30 12:58:21 +1000

    new model with mvc

---
+++ Team28Delivery/Team28Delivery/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Team28Delivery.Startup))]
+namespace Team28Delivery
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
