88fbce82701d86194cf97afe3cffbc42f010a859
Harrison McColl <hazel@hotmail.com.au>
2016-09-19 09:50:34 +1000

    merged Harry's work

---
+++ Team28Delivery/Team28Delivery/Controllers/CustomerUserController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using Team28Delivery.Models;
+
+namespace Team28Delivery.Controllers
+{
+    public class CustomerUserController : Controller
+    {
+        // GET: CustomerUser
+        public ActionResult Index()
+        {
+            string constr = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            string sql = "SELECT * FROM [dbo].[AspNetUsers]";
+            var model = new List<CustomerUser>();
+
+            using (SqlConnection conn = new SqlConnection(constr))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    var customeruser = new CustomerUser();
+                    customeruser.FirstName = rdr["FirstName"].ToString();
+                    customeruser.LastName = rdr["LastName"].ToString();
+                    customeruser.Phone = rdr["Phone"].ToString();
+                    customeruser.Email = rdr["Email"].ToString();
+                    //customeruser.UserType = rdr["UserType"].ToString();
+
+                    //customeruser.StreetAddress = rdr["StreetAddress"].ToString();
+                    //customeruser.Suburb = rdr["Suburb"].ToString();
+                    //customeruser.PostalCode = rdr["PostalCode"].ToString();
+                    //customeruser.State = rdr["State"].ToString();
+                    //customeruser.Country = rdr["Country"].ToString();
+                    model.Add(customeruser);
+                }
+            }
+            return View(model);
+        }
+
+    }
+}
---
+++ Team28Delivery/Team28Delivery/Models/CustomerUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team28Delivery.Models
+{
+    public class CustomerUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string UserType { get; set; }
+
+        public string StreetAddress { get; set; }
+        public string Suburb { get; set; }
+        public string PostalCode { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+    }
+}
